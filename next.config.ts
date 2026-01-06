c5f68755d2c321b2f260c21e56b73798cdb66a47
Roshan Kumar <roshankumar@Roshans-MacBook-Air.local>
2026-01-07 00:19:34 +0530

    Feat: Add legacy URL redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:slug*-Pin-Code-:pincode(\\d{6})',
+        destination: '/pincode/:pincode',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
